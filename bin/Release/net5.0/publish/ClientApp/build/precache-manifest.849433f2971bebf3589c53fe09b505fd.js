3903bc811a0904a4439618164c6fd30e80b7e154
Chris Frewin <frewin.christopher@gmail.com>
2020-11-24 10:38:24 -0500

    build too....

---
+++ bin/Release/net5.0/publish/ClientApp/build/precache-manifest.849433f2971bebf3589c53fe09b505fd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f078c5aae813f6bd162bcf27d4051692",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4ef171d7618dcf2c2397",
+    "url": "/static/css/2.74189dbb.chunk.css"
+  },
+  {
+    "revision": "775e0aafcb748a5c4774",
+    "url": "/static/css/main.dcfde02c.chunk.css"
+  },
+  {
+    "revision": "4ef171d7618dcf2c2397",
+    "url": "/static/js/2.91e74ecf.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.91e74ecf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "775e0aafcb748a5c4774",
+    "url": "/static/js/main.66cb10f9.chunk.js"
+  },
+  {
+    "revision": "119040cadb622b354adb",
+    "url": "/static/js/runtime-main.739d7fd2.js"
+  }
+]);
